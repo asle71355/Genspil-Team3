f07ff2b822047553e42de860ee1a8c54a009efe8
Freja <fbfl71372@edu.ucl.dk>
2025-03-26 16:54:12 +0100

    Man kan nu tilføje en kunde. Jeg har udkommenteret det for nu, så man ikke behøver at skrive det ind hver gang man kører programmet.

--- GenspilApp/GenspilAPP/Program.cs
+++ GenspilApp/GenspilAPP/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Xml.Linq;
 
 namespace GenspilApp
 {
@@ -8,15 +10,19 @@ namespace GenspilApp
         {
             Console.WriteLine("Hello, World!");
 
-            //Reservation
-            Reservation.MakeReservation();
-
             //Customer
-            Customer customer = new Customer();
-            customer.AddReservations();
-            customer.RemoveReservation();
-            customer.GetReservations();
-     
+            //Console.WriteLine("Tilføj kunde");
+            //Console.Write("Indtast kundens navn: ");
+            //string name = Console.ReadLine();
+
+            //Console.Write("Indtast telefonnummer: ");
+            //int telephoneNum = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Indtast kundens addresse: ");
+            //string address = Console.ReadLine();
+
+            //Customer customer = new Customer(name, telephoneNum, address);
+
             Console.ReadLine();
         }
     }
