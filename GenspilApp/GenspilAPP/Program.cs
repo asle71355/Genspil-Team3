5f577049acac939d128512795b31d692cb2c1dac
Freja <fbfl71372@edu.ucl.dk>
2025-03-27 13:51:15 +0100

    Besked

--- GenspilApp/GenspilAPP/Program.cs
+++ GenspilApp/GenspilAPP/Program.cs
@@ -7,10 +7,23 @@ namespace GenspilApp
         static void Main(string[] args)
         {
 
-            Boardgame myGame = new Boardgame("Risk", "2-8", new List<Genre> { Genre.Action, Genre.Strategy });
-            myGame.AddBoardgameVariants(new BoardgameVariant("risk classic", 500, "Dansk", Status.Good, State.InStock));
+            //Boardgame myGame = new Boardgame("Risk", "2-8", new List<Genre> { Genre.Action, Genre.Strategy });
+            //myGame.AddBoardgameVariants(new BoardgameVariant("risk classic", 500, "Dansk", Status.Good, State.InStock));
 
             //MenuClass.Menu(MainMenu.menuOptions, MainMenu.DisplayMenu);
+
+            //Customer
+            Customer customer = new Customer("Name", 12345678, "Address");
+
+            Console.Write("Indtast kundens navn: ");
+            string name = Console.ReadLine();
+            customer.SetName(name);
+
+            Console.Write("Indtast telefonnummer: ");
+            int telephoneNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Indtast kundens addresse: ");
+            string address = Console.ReadLine();
 
 
             Console.ReadLine();
