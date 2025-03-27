21efafcd72a049f6d29ca8463eaa5593a4b1355a
Nicolai Friis <nmfr71398@edu.ucl.dk>
2025-03-27 15:22:58 +0100

    Formatting

--- GenspilApp/GenspilAPP/Boardgame.cs
+++ GenspilApp/GenspilAPP/Boardgame.cs
@@ -44,7 +44,7 @@ namespace GenspilApp
                 }
                 else
                 {
-                    throw new ArgumentException("The field cannot be empty, please enter a name..");
+                    throw new ArgumentException("Feltet må ikke være tomt..");
                 }
             }
         }
@@ -59,7 +59,7 @@ namespace GenspilApp
                 }
                 else
                 {
-                    throw new ArgumentException("The number of players must be greater than 0..");
+                    throw new ArgumentException("Der skal være mere end 0 spillere..");
                 }
 
             }
@@ -71,30 +71,11 @@ namespace GenspilApp
             {
                 if (value == null || value.Count == 0)
                 {
-                    throw new ArgumentException("The game must have at least one genre..");
+                    throw new ArgumentException("Spillet skal have mindst en genre..");
                 }
                 _genre = value;
             }
-        }
-
-        public List<BoardgameVariant> LoadBoardgameVariants()
-        {
-            try
-            {
-                if (File.Exists($"{Name}.txt"))
-                {
-                    return BoardgameVariant = File.ReadAllLines($"{Name}.txt").Select(line => line.Split(";"))
-                    .Select(bV => new BoardgameVariant(bV[0], double.Parse(bV[1]), bV[2],
-                    (Status)Convert.ToInt32(bV[3]), (State)Convert.ToInt32(bV[4]))).ToList();
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Fandt ikke filen");
-            }
-            return null;
-        }
-
+        }        
         public List<BoardgameVariant> BoardgameVariant
         {
             get { return _boardgameVariants; }
@@ -126,6 +107,24 @@ namespace GenspilApp
             }
 
             File.AppendAllText("Boardgame.txt", boardgame.Name + ";" + boardgame.Players + ";" + string.Join(",", boardgame.Genre.Select(g => ((int)g))) + "\n");
+        }
+
+        public List<BoardgameVariant> LoadBoardgameVariants()
+        {
+            try
+            {
+                if (File.Exists($"{Name}.txt"))
+                {
+                    return BoardgameVariant = File.ReadAllLines($"{Name}.txt").Select(line => line.Split(";"))
+                    .Select(bV => new BoardgameVariant(bV[0], double.Parse(bV[1]), bV[2],
+                    (Status)Convert.ToInt32(bV[3]), (State)Convert.ToInt32(bV[4]))).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fandt ikke filen");
+            }
+            return null;
         }
 
         public void PrintBoardGame()
