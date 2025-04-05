2d729634cb65daef264ae59b5d2050efa0973fb4
ayman-a275 <ayman.dk01@gmail.com>
2025-04-05 13:44:36 +0200

    Tilføjet metoder til CustomerMenu

--- GenspilApp/GenspilAPP/Menu/CustomerMenu.cs
+++ GenspilApp/GenspilAPP/Menu/CustomerMenu.cs
@@ -10,9 +10,73 @@ namespace GenspilApp.Menu
     {
         public static Dictionary<int, (Action, string)> menuOptions = new()
             {
-                {1, (() => {}, "Se kunder")},
-                {2, (() => {}, "Opret ny kunde")},
-                { 3, (() => {}, "Slet kunde") },
+                {1, (() => CustomerSortedByName(), "Se kunder")},
+                {2, (() => AddCustomer(), "Opret ny kunde")},
+                { 3, (() => RemoveCustomer(), "Slet kunde") },
             };
+
+        public static void AddCustomer()
+        {
+
+            StringBuilder log = new();
+            Console.Clear();
+            MenuClass.Log(log, @"---Genspil----
+Opret ny kunde
+Kundens navn: ");
+            string name = Console.ReadLine();
+            MenuClass.Log(log, name, false);
+
+            MenuClass.Log(log, "Kundens telefon nummer: ");
+            string telNum = Console.ReadLine();
+            MenuClass.Log(log, telNum, false);
+
+        }
+
+        public static void CustomerSortedByName()
+        {
+            Console.Clear();
+
+            Console.WriteLine($@"---Genspil---
+Kunder sorteret efter navn
+
+Brug Esc til at lukke programmet.
+Brug Backspace til at gå tilbage til hovedmenuen.
+
+
+            ");
+
+            var SortedBoardgame = Storage.Storage.boardgames.OrderBy(b => b.Name);
+
+            foreach (Boardgame boardgame in SortedBoardgame)
+            {
+                Console.WriteLine($"Title: {boardgame.Name}; Antal spillere: {boardgame.Players}; Genrer: {string.Join(", ", boardgame.Genre)}");
+            }
+        }
+
+        public static void RemoveCustomer()
+        {
+            StringBuilder log = new();
+            Dictionary<int, string> removeBoardgameMenuOptions = Storage.Storage.boardgamesDict;
+            int counter = 1;
+
+            Console.Clear();
+            MenuClass.Log(log, $@"---Genspil---
+Slet brætspil
+Brug piletasterne og Enter til at vælge et menupunkt.
+Brug Esc til at lukke programmet.
+Brug Backspace til at gå tilbage til hovedmenuen.
+
+Vælg et brætspil der skal sletters.
+");
+
+            string name = MenuClass.MenuItems(removeBoardgameMenuOptions, log, 1);
+
+            Boardgame boardgameToBeRemoved = Storage.Storage.boardgames.Find(b => b.Name == name);
+
+            Storage.Storage.Removeboardgame(boardgameToBeRemoved);
+
+            MenuClass.Menu(MainMenu.menuOptions, "Menu", 1);
+
+        }
     }
 }
