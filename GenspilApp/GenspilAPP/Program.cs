1cb3eee9f17fd2eff813fbfddb213d4b827113e5
ayman-a275 <ayman.dk01@gmail.com>
2025-04-08 11:12:35 +0200

    slettet ubrugt kode

--- GenspilApp/GenspilAPP/Program.cs
+++ GenspilApp/GenspilAPP/Program.cs
@@ -6,28 +6,6 @@ namespace GenspilApp
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Hvad er dit boardgame navn");
-            string boardgameNavn = Console.ReadLine();
-            Console.WriteLine("Hvor mange spillere kan der være");
-            string spillerAntal = Console.ReadLine();
-            Boardgame myGame = new Boardgame(boardgameNavn, spillerAntal, new List<Genre> { Genre.Action, Genre.Strategy });
-            Console.Write("Vælg navn");
-            string navn = Console.ReadLine();
-            if(navn == "x")
-            {
-                foreach(BoardgameVariant variant in myGame.BoardgameVariant)
-                {
-                    Console.WriteLine(variant.Print());
-                    
-                }
-            }
-            Console.WriteLine("Hvad er prisen");
-            double pris = Convert.ToDouble(Console.ReadLine());
-            
-            myGame.AddBoardgameVariants(new BoardgameVariant(navn, pris, "Dansk", State.Good, Status.InStock));
-            */
-
 
             Storage.Storage.LoadBoardgameFile();
             Storage.Storage.CreateBoardgamesDictionary();
@@ -37,21 +15,6 @@ namespace GenspilApp
             Storage.Storage.CreateEnumDictionary<Status>(Storage.Storage.StatusDict);
             Storage.Storage.CreateEnumDictionary<State>(Storage.Storage.StateDict);
             MenuClass.Menu(MainMenu.menuOptions, "Menu", 1);
-
-
-
-            //Customer
-            //Customer customer = new Customer("Name", 12345678, "Address");
-
-            //Console.Write("Indtast kundens navn: ");
-            //string name = Console.ReadLine();
-            //customer.SetName(name);
-
-            //Console.Write("Indtast telefonnummer: ");
-            //int telephoneNum = Convert.ToInt32(Console.ReadLine());
-
-            //Console.Write("Indtast kundens addresse: ");
-            //string address = Console.ReadLine();
 
 
             Console.ReadLine();
